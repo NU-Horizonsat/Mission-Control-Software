ace72823ab223674921a5bd1ffe896a761165256
aku7703 <31360520+aku7703@users.noreply.github.com>
2025-02-12 23:35:27 -0500

    Fixed some dependencies to allow Docker to properly run containers

--- openmct/server.js
+++ openmct/server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public'))); // Where OpenMCT build might live
 
-// If you want to serve index.html directly:
+// 1) Serve node_modules so /node_modules/openmct/dist/openmct.js is accessible
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+
+// 2) Serve your plugin scripts if they are in the same folder
+app.use(express.static(__dirname)); 
+// or app.use('/', express.static(path.join(__dirname, 'public')));
+
+// 3) If you have an index.html in this same directory
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
